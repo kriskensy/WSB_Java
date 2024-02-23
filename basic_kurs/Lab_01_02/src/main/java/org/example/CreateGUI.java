14ebbc7ac3621bd8fee1c4d83f80b98937398abd
kriskensy <kriskensy@proton.me>
2024-02-23 20:08:09 +0100

    First Lab, GUI

---
+++ basic_kurs/Lab_01_02/src/main/java/org/example/CreateGUI.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class CreateGUI {
+
+    public void main(){
+        JFrame frame = new JFrame();
+        frame.setSize(640,480);
+        frame.setVisible(true);
+    }
+}
---
+++ basic_kurs/Lab_01_02/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CreateGUI createGUI = new CreateGUI(); //tworze obiekt klasy CreateGUI
+
+        createGUI.main(); //na obiekcie klasy CreatGUI wywoluje metode main()
+    }
+}
