eff8962cfdf8122564e8b4c2f30e20499d0a7bf5
kriskensy <kriskensy@proton.me>
2024-06-05 21:35:09 +0200

    Lab_06 homework_1. Swing interface + database (add, edit, delete from table)

---
+++ mid_kurs/Lab_06_homework_1/src/main/java/pl/kriskensy/Main.java
@@ -0,0 +1,39 @@
+package pl.kriskensy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/students_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection established!");
+
+        } catch (SQLException e) {
+            System.out.println("Connection do DB impossible.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+
+        Connection connection = getConnection();
+        if (connection != null) {
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
